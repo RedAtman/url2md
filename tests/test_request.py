de7ff0db374b1d6744c18cf4906ce65e160e8fa1
nut <evicwork@gmail.com>
2024-01-14 23:43:28 +0800

    test: Refactor test_request method in ExampleTest

--- tests/test_request.py
+++ tests/test_request.py
@@ -8,29 +8,27 @@ from utils.response import Result
 class ExampleTest(BaseTest):
 
     def test_request(self):
-        # python apps/manage.py test apps.common.basetest.ExampleTest.test_health
-        url = 'https://segmentfault.com/a/1190000021854441'
-        # url = 'https://www.baidu.com/'
+        '''python -m unittest tests.test_request.ExampleTest.test_request
+        '''
         # url = 'https://httpbin.org/get'
-        # url = 'https://www.runoob.com/go/go-fmt-sprintf.html'
+        # url = 'https://www.baidu.com/'
+        url = 'https://segmentfault.com/a/1190000021854441'
+        url = 'https://www.runoob.com/go/go-fmt-sprintf.html'
         result: Result = self.request(
             url=url,
             method='GET',
             data={}
         )
+        assert isinstance(result, Result)
         assert result.code == 200
-        logger.json(result.data)
         assert isinstance(result.__dict__, dict)
         assert isinstance(result.data, str)
-        logger.json(type(result.data))
-        # logger.json(json.loads(result.data))
-        assert isinstance(result, Result)
         logger.json(result.__class__)
+        logger.json(type(result.data))
+        logger.json(result)
+        # logger.json(json.loads(result.data))
         # assert False
-        # return response
 
 
 if __name__ == '__main__':
-    # example = ExampleTest()
-    # example.test_request()
     unittest.main(verbosity=2)
