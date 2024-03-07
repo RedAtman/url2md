2b6f9ebd9f2d72f5029e3b443c01c33900b4b766
nut <evicwork@gmail.com>
2024-03-08 00:56:45 +0800

    test: Refactor test_request method in test_request.py

--- tests/test_request.py
+++ tests/test_request.py
@@ -6,19 +6,14 @@ from utils.response import Result
 
 
 class ExampleTest(BaseTest):
-
     def test_request(self):
-        '''python -m unittest tests.test_request.ExampleTest.test_request
-        '''
+        """python -m unittest tests.test_request.ExampleTest.test_request"""
+
         # url = 'https://httpbin.org/get'
         # url = 'https://www.baidu.com/'
-        url = 'https://segmentfault.com/a/1190000021854441'
-        url = 'https://www.runoob.com/go/go-fmt-sprintf.html'
-        result: Result = self.request(
-            url=url,
-            method='GET',
-            data={}
-        )
+        url = "https://segmentfault.com/a/1190000021854441"
+        url = "https://www.runoob.com/go/go-fmt-sprintf.html"
+        result: Result = self.request(url=url, method="GET", data={})
         assert isinstance(result, Result)
         assert result.code == 200
         assert isinstance(result.__dict__, dict)
@@ -30,5 +25,5 @@ class ExampleTest(BaseTest):
         # assert False
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main(verbosity=2)
